23f23419f85bdf56875161864a95952b33f74400
+ <andrewjohnjulian@gmail.com>
2023-12-18 15:03:45 -0700

    url scraper

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  url: "https://rooms.xyz/docs",
+  match: "https://rooms.xyz/docs#**",
+  maxPagesToCrawl: 100,
   outputFileName: "output.json",
 };
